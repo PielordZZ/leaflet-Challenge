35f518fb57128a52b4213652f5c714802c403226
PielordZZ <zzpieme@gmail.com>
2022-04-11 20:43:39 -0400

    created functions for marker color and size

--- WorkableCode/static/js/logic.js
+++ WorkableCode/static/js/logic.js
@@ -14,13 +14,36 @@ var streetmap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(myMap);
 
+function circleSize(mag){
+  return mag**5 *10000
+};
+function colorCircle(mag){
+  var color= 'green';
+  if(mag>6){
+    color = 'red';
+  }
+  else if(mag>3.5){
+    color='yellow'
+  }
+  else {
+    color = 'green';
+  };
+  return color
+};
+
 d3.json(url).then(url, function(data){
     console.log(data)
     for (var i = 0; i < response.length.features; i++) {
         var location = data.features[i].geometry.coordinates;
         console.log(location)
+        var magnitude = data[i].properties.mag
         if (location) {
-          L.marker([location.coordinates[1], location.coordinates[0]]).addTo(myMap);
+          L.circle([location.coordinates[1], location.coordinates[0]], {
+            color: colorCircle(mag),
+            fillColor: colorCircle(mag),
+            fillOpacity: 0.5,
+            radius: circleSize(mag)
+          }).addTo(myMap);
         }
       } 
 });
